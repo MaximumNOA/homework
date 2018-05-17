05d9c107f5110d72c3437d29383ae8709ab40222
Kondrashov Maxim <kondrashov10@gmail.com>
2018-05-17 21:19:46 +0300

    js1

--- public/views/javascript/howework-1/script.js
+++ public/views/javascript/howework-1/script.js
@@ -10,5 +10,17 @@ admin = name;
 console.log(admin);
 
 //the third task
-var planetEarth = 'Земля';
+var planetName = 'Земля';
 var userName = 'Петя';
+
+//fourth task
+// Придумать названия переменных для следующего: 
+// количество кадров в секунду; 
+// соеденительный знак между словами; 
+// режим просмотра документа; 
+// необходимость показа последней страницы постраничной навигации.
+
+var fps;
+var blank;
+var viewDocumnet;
+var needViewLastPageNavigation;
