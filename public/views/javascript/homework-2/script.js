613c3d10a76eaa19b205ac7757d582b0b6850258
Kondrashov Maxim <kondrashov10@gmail.com>
2018-05-17 20:26:07 +0300

    js

---
+++ public/views/javascript/homework-2/script.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var a = 2; var b = 4; var c = (b += 5) - a++ * b - 3 + (b -= 3 + 1 - —a) alert(c);
---
+++ public/views/javascript/howework-1/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//first task
+alert('Я - ™');
+
+//second task
+var admin, name; 
+name = 'Василий';
+admin = name;
+console.log(admin);
+
+//the third task
+var planetEarth = 'Земля';
+var userName = 'Петя';
